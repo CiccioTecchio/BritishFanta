0fae9b566a25832f2e5d5a13dffbd6c5f1cd9eb1
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2018-05-16 23:20:07 +0200

    added error.html &  restailing blanck.html

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const createError = require('http-errors');
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const playerCNT = require('./controller/playerCNT.js');
 const userCNT = require('./controller/userCNT.js');
-const fs = require('fs');
 let app = express();
 
 let dbUrl = "mongodb://localhost:27017/dbpl";
@@ -38,7 +34,8 @@ app.post('/registrazione', function (req, res) {
     userCNT.insertOne(res, req.body);
 });
 
-let server = app.listen(8081, function () {
+let server = app.listen(8081, "127.0.0.1", function () {
+    let address = server.address().address;
     let port = server.address().port;
-    console.log("Listening on " + port);
+    console.log("Listening on "+address+":"+port);
 });
